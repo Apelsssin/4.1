3fdfe7c35237c096f1641f0982eebfc1f2de8933
Zhitikhina Mariia <zhitikhinamv@gmail.com>
2023-07-12 17:52:43 +0300

    first commit

---
+++ 4.1.1/4.1.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::vector<int> V = { 4, 7, 9, 14, 12 };
+
+    std::cout << "Входные данные: ";
+    for (int& it : V) {
+        std::cout << it << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Выходные данные: ";
+    [&V]() {
+        for (int& it : V) {
+            if (it % 2 != 0)
+                it = it * 3;
+            std::cout << it << " ";
+        }
+        std::cout << std::endl;
+    }();
+}
